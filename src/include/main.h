5c84cdbfed8057d0e9496d92af185f7568bdbbff
Patrick Pawelczyk <p4czyk@outlook.com>
2023-11-17 13:10:27 +0100

    Fix previous merge conflict

--- src/include/main.h
+++ src/include/main.h
@@ -5,10 +5,7 @@
 #include "msum.h"
 #include "leftlayerone.h"
 #include "miscellaneous.h"
-<<<<<<< HEAD
 #include "metaheuristic1.h"
-=======
->>>>>>> main
 
 bool visualize = false;
 int algorithm = 0;
