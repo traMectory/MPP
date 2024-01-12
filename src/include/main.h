df8b5f830e80a3ccdb03eac948202771f842631b
Jona <jonalukas@web.de>
2024-01-12 12:22:04 +0100

    readded includes to header

--- src/include/main.h
+++ src/include/main.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <iostream>
+
 #include "problem.hpp"
-//#include "msum.h"
-//#include "leftlayerone.h"
+#include "msum.h"
+#include "leftlayerone.h"
 #include "miscellaneous.h"
-//#include "toposv2.h"
-//#include "geneticalgorithm.h"
+#include "toposv2.h"
+#include "geneticalgorithm.h"
 #include "incrementalnofitsolver.h"
 
-bool visualize = true;
+bool visualize = false;
 int algorithm = 4;
